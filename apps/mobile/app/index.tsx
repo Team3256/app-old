0e51214659c3a315b2f0b834e7c262b9c2d0c324
Josh <102478723+realjoshuau@users.noreply.github.com>
2023-11-23 17:39:02 -0600

    Add camera permission handling and PIN functionality

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -2,10 +2,11 @@
 // import { HomeIcon, HomeScreen } from '@acme/feature-home';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Image, Button } from 'react-native';
+import { StyleSheet, View, Text, Image, Button, Linking, Alert } from 'react-native';
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [message, setMessage] = useState('No PIN entered.');
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -16,17 +17,52 @@ export default function App() {
     getBarCodeScannerPermissions();
   }, []);
 
+  if (hasPermission === null) {
+    return (
+      <>
+        <View style={styles.container}>
+          <Text>Requesting for camera permission</Text>
+          <Button
+            title={'Enter Override PIN'}
+            onPress={() => {
+              Alert.alert('Not implemented.', "This feature hasn't been implemented yet.");
+            }}
+          />
+
+          <Text>{message}</Text>
+        </View>
+      </>
+    );
+  }
+  if (hasPermission === false) {
+    return (
+      <>
+        <View style={styles.container}>
+          <Text>No access to camera</Text>
+          <Button
+            title={'Open Settings'}
+            onPress={() => {
+              // Launch settings for the app
+              Linking.openSettings();
+            }}
+          />
+          <Button title={'Try Again'} onPress={() => setHasPermission(null)} />
+          <Button
+            title={'Enter Override PIN'}
+            onPress={() => {
+              Alert.alert('Not implemented.', "This feature hasn't been implemented yet.");
+            }}
+          />
+          <Text>{message}</Text>
+        </View>
+      </>
+    );
+  }
+
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
 
   return (
     <View style={styles.container}>
@@ -46,6 +82,8 @@ export default function App() {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
+      <Text>Has permission: {hasPermission.toString()}</Text>
+
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
